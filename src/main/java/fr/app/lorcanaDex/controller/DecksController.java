94a90ef94403aa9d0a33c7abd1682f337b860d50
Tanguy RAGUENES <tanguy.raguenes2024@campus-eni.fr>
2024-10-18 16:54:38 +0200

    test

--- src/main/java/fr/app/lorcanaDex/controller/DecksController.java
+++ src/main/java/fr/app/lorcanaDex/controller/DecksController.java
@@ -2,18 +2,25 @@ package fr.app.lorcanaDex.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/decks")
 public class DecksController {
 
-    @PostMapping("/")
-    public void addDeckToBdd() {
+    @PostMapping("")
+    public String addDeckToBdd() {
 
+        System.out.println("Coucou du back");
+        return "Coucou du back";
     }
 
-    @GetMapping("/")
-    public void getDecksByUserName() {
+    @GetMapping("")
+    public String getDecksByUserName() {
+
+        System.out.println("Coucou du back");
+        return "Coucou du back";
 
     }
 
