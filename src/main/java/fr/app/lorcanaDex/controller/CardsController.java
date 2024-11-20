fa6a7125ad87fd381187d8332964649e7b64f736
TanguyRaguenes <tanguy.raguenes@gmail.com>
2024-11-20 13:54:03 +0100

    Nouvelle api

--- src/main/java/fr/app/lorcanaDex/controller/CardsController.java
+++ src/main/java/fr/app/lorcanaDex/controller/CardsController.java
@@ -41,7 +41,8 @@ public class CardsController {
 
         List<SetApiLorcast> sets = builder.build()
                 .get()
-                .uri(environment.getProperty("lorcast.Url.GetSets"))
+                // .uri(environment.getProperty("lorcast.Url.GetSets"))
+                .uri("https://api.lorcast.com/v0/sets")
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, List<SetApiLorcast>>>() {
                 })
@@ -52,7 +53,8 @@ public class CardsController {
 
             List<CardApiLorcast> setCards = builder.build()
                     .get()
-                    .uri(environment.getProperty("lorcast.Url.GetCardsBySet"), set.getCode())
+                    // .uri(environment.getProperty("lorcast.Url.GetCardsBySet"), set.getCode())
+                    .uri("https://api.lorcast.com/v0/sets/{code}/cards", set.getCode())
                     .retrieve()
                     .bodyToFlux(CardApiLorcast.class)
                     .collectList()
